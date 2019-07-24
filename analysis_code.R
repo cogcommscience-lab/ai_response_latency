f9a3e932558c904c35fcefd99c1d164565385294
Richard Huskey <huskey.richard@gmail.com>
2019-07-24 11:24:01 -0700

    cleaned up code for plotting 3-way interaction

--- analysis_code.R
+++ analysis_code.R
@@ -1,9 +1,17 @@
+
 # These two guides were helpful
 # http://www.sthda.com/english/wiki/two-way-anova-test-in-r
 # https://www.r-bloggers.com/raccoon-ch-2-4-3-way-anova/
 
 # Info on how to interpret the QC plots
 # https://data.library.virginia.edu/diagnostic-plots/
+
+# Load packages
+library(dplyr)
+library(ggplot2)
+library(ggridges)
+library(plotrix)
+library(car)
 
 # Load in data
 ai_data<- read.csv(file.choose())
@@ -39,16 +47,17 @@ aov_residuals <- residuals(object = fm)
 # Run Shapiro-Wilk test
 shapiro.test(x = aov_residuals )
 
-
-
 # Get some descriptive statistics for the cells
-require(dplyr)
-group_by(ai_data, Modality, OS, Python) %>%
+
+dsummary<-group_by(ai_data, Modality, OS, Python) %>%
   summarise(
     count = n(),
     mean = mean(RT, na.rm = TRUE),
-    sd = sd(RT, na.rm = TRUE)
+    sd = sd(RT, na.rm = TRUE),
+    se = std.error(RT, na.rm = TRUE),
+    ci95 = 1.96*(std.error(RT, na.rm=TRUE))
   )
+dsummary
 
 # Run some post-hoc comparisons
 TukeyHSD(fm, which = "OS")
@@ -56,34 +65,53 @@ TukeyHSD(fm, which = "Python")
 TukeyHSD(fm, which = "Modality")
 
 
-library(ggplot2)
-
-
-# Plot the cell means for the three way interaction
-cell_means <- aggregate(RT ~ Modality + OS + Python, ai_data, mean)
-
-ggplot(cell_means,
-       aes(Modality, RT, linetype = Python, shape = Python, group = Python)) +
-  geom_line() + geom_point(size = 3) +
-  facet_wrap(~OS)
-
-library(ggridges)
+# Make raincloud plots for main effects
 
 ggplot(ai_data, aes(x=RT, y=OS, color=OS)) +
   geom_density_ridges(
     jittered_points = TRUE, position = "raincloud",
     alpha = 0.7, scale = 0.7
-  )
-
+  ) +
+  labs(title = "Raincloud Plot For Operating System")
 
 ggplot(ai_data, aes(x=RT, y=Python, color=Python)) +
   geom_density_ridges(
     jittered_points = TRUE, position = "raincloud",
     alpha = 0.7, scale = 0.7
-  )
+  ) +
+  labs(title = "Raincloud Plot For Python Version")
 
 ggplot(ai_data, aes(x=RT, y=Modality, color=Modality)) +
   geom_density_ridges(
     jittered_points = TRUE, position = "raincloud",
     alpha = 0.7, scale = 0.7
-  )
+  ) +
+  labs(title = "Raincloud Plot For Modality")
+
+# Plot two-way interactions
+# See https://homepage.divms.uiowa.edu/~luke/classes/STAT4580/boxetc.html
+
+ggplot(ai_data, aes(x=RT, y=Python, group=interaction(OS, Python), fill=OS)) +
+  geom_density_ridges(
+    jittered_points = TRUE, position = "raincloud",
+    alpha = 0.7, scale = 0.6
+  ) +
+  labs(title = "Raincloud Plot For Operating System by Python Interaction")
+
+ggplot(ai_data, aes(x=RT, y=Modality, group=interaction(OS, Modality), fill=OS)) +
+  geom_density_ridges(
+    jittered_points = TRUE, position = "raincloud",
+    alpha = 0.7, scale = 0.6
+  ) +
+  labs(title = "Raincloud Plot For Operating System by Modality Interaction")
+
+
+# Plot the three way interaction with 95% CI error bars
+
+ggplot(dsummary,
+       aes(Modality, mean, linetype = Python, shape = Python, group = Python, color=Python)) +
+  geom_line() + geom_point(size = 3) +
+  facet_wrap(~OS) +
+  geom_errorbar(aes(ymin=mean-ci95, ymax=mean+ci95), width=.2) +
+  labs(title = "Three Way Interaction",
+       subtitle = "Error Bars Represent 95% CI of the Mean")
